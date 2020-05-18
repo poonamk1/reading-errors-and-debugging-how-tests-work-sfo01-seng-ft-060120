f9d9d233c2973878f1415ba92d00c474e203835c
punam kwatra <punam2005@hotmail.com>
2020-05-18 21:43:41 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 first_number= 50
-second_number= 0
-#ssum = first_number + second_number
-# difference = first_number - second_number
-#product = first_number * second_number
-#quotient = first_number / second_number
+second_number= 20
+sum = first_number + second_number
+ difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
